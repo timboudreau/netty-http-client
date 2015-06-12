3cdb530c5967f2eab01c02ed8c50aca4059ceeba
Tim Boudreau <niftiness@gmail.com>
2015-06-12 03:26:52 -0400

    Use Netty's newer API for setting up SSL connections; incompatible change to accept Netty's SSLContext rather than Java's SSLContext

--- netty-http-client/src/test/java/com/mastfrog/netty/http/client/ConnectionRefusedTest.java
+++ netty-http-client/src/test/java/com/mastfrog/netty/http/client/ConnectionRefusedTest.java
@@ -78,7 +78,7 @@ public class ConnectionRefusedTest {
         final CountDownLatch latch = new CountDownLatch(1);
         HttpClient client = HttpClient.builder().setTimeout(Duration.standardSeconds(3)).build();
         final AtomicBoolean notified = new AtomicBoolean();
-        client.get().setURL("http://10.0.0.0/abcd")
+        client.get().setURL("http://192.168.1.254:10001/abcd")
                 .onEvent(new Receiver<State<?>>() {
 
                     @Override
@@ -116,7 +116,9 @@ public class ConnectionRefusedTest {
                     }
                 }).await(1, TimeUnit.SECONDS);
         latch.await(20, TimeUnit.SECONDS);
-        Thread.sleep(5000);
+        if (!notified.get()) {
+            Thread.sleep(5000);
+        }
         assertTrue(notified.get());
     }
 }
